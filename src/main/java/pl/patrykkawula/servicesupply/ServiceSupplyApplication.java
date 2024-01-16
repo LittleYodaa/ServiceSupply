fda0ec98029dc1c56490fc985922860aae6f1cd0
Patryk Kawula <kawul1996@gmail.com>
2024-01-16 16:48:55 +0100

    Start project

---
+++ src/main/java/pl/patrykkawula/servicesupply/ServiceSupplyApplication.java
@@ -0,0 +1,13 @@
+package pl.patrykkawula.servicesupply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceSupplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceSupplyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/patrykkawula/servicesupply/ServiceSupplyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.patrykkawula.servicesupply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceSupplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
