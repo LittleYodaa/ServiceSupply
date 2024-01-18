f5aaef9221ff14dd12582eed3b4418de2612cb83
Patryk Kawula <kawul1996@gmail.com>
2024-01-18 11:07:09 +0100

    Change external port from 8080 to 20289

--- build-and-restart.sh
+++ build-and-restart.sh
@@ -1,4 +1,4 @@
 docker build -t servicesupply .
 docker stop servicesupply || true
 docker rm servicesupply || true
-docker run -d -p 8080:8080 --name servicesupply servicesupply
+docker run -d -p 20289:8080 --name servicesupply servicesupply
--- src/main/java/pl/patrykkawula/servicesupply/ServiceSupplyApplication.java
+++ src/main/java/pl/patrykkawula/servicesupply/ServiceSupplyApplication.java
@@ -9,5 +9,4 @@ public class ServiceSupplyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceSupplyApplication.class, args);
 	}
-
 }
