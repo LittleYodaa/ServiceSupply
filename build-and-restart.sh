2214a426c8f0ce3329897d875fb0d6160cafd536
Patryk Kawula <kawul1996@gmail.com>
2024-01-25 19:04:26 +0100

    Change prod profile to connect external database

--- build-and-restart.sh
+++ build-and-restart.sh
@@ -1,4 +1,4 @@
 docker build -t servicesupply .
 docker stop servicesupply || true
 docker rm servicesupply || true
-docker run -d -p 20289:8080 --name servicesupply -e SPRING_PROFILES_ACTIVE=dev --restart unless-stopped servicesupply
+docker run -d -p 20289:8080 --name servicesupply -e SPRING_PROFILES_ACTIVE=prod --restart unless-stopped servicesupply
