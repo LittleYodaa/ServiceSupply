10bebc73b470a756ad4147ae7892920a46b20411
Patryk Kawula <kawul1996@gmail.com>
2024-02-05 20:00:03 +0100

    Add ControllerAdvisor with @ControllerAdvice to global exception handle. @ExceptionHandler send excexption to default_error_view template with hidden info about exception to help support.

---
+++ src/main/java/pl/patrykkawula/servicesupply/exception/ControllerAdvisor.java
@@ -0,0 +1,38 @@
+package pl.patrykkawula.servicesupply.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+class ControllerAdvisor {
+
+    @ExceptionHandler(ProductDetailsNotFoundException.class)
+    public String handle(HttpServletRequest request,
+                               ProductDetailsNotFoundException exception, Model model) {
+        ModelAndView mav = new ModelAndView();
+        model.addAttribute("exception", exception);
+        model.addAttribute("url", request.getRequestURL());
+        return "/errorView/default_error_view";
+    }
+
+    @ExceptionHandler(ProductDetailsNotFoundException.class)
+    public String handle(HttpServletRequest request,
+                         BrandNotFoundException exception, Model model) {
+        ModelAndView mav = new ModelAndView();
+        model.addAttribute("exception", exception);
+        model.addAttribute("url", request.getRequestURL());
+        return "/errorView/default_error_view";
+    }
+
+    @ExceptionHandler(ProductDetailsNotFoundException.class)
+    public String handle(HttpServletRequest request,
+                         StoreNotFoundException exception, Model model) {
+        ModelAndView mav = new ModelAndView();
+        model.addAttribute("exception", exception);
+        model.addAttribute("url", request.getRequestURL());
+        return "/errorView/default_error_view";
+    }
+}
